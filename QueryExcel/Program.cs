dfa2fcd2abaa12f6e8dba82e0b710619768ada83
KaryoYou <62101443+KaryoYou@users.noreply.github.com>
2023-09-15 22:01:09 +0800

    创建项目

--- QueryExcel/Program.cs
+++ QueryExcel/Program.cs
@@ -1,6 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
+namespace QueryExcel
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
