bd5be18b06365a2e954864cd41aea4be264ff469
KaryoYou <62101443+KaryoYou@users.noreply.github.com>
2023-09-22 16:52:52 +0800

    修正代码
    修正内容:自定义筛选条件的判断代码.

--- QueryExcel/Form1.cs
+++ QueryExcel/Form1.cs
@@ -23,10 +23,22 @@ namespace QueryExcel
         /// <param name="e"></param>
         private void Form1_DragEnter(object sender, DragEventArgs e) //获得“信息”
         {
-            if (e.Data.GetDataPresent(DataFormats.FileDrop))
-                e.Effect = DragDropEffects.All; //重要代码：表明是所有类型的数据，比如文件路径
-            else
-                e.Effect = DragDropEffects.None;
+            try
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    e.Effect = DragDropEffects.All; //重要代码：表明是所有类型的数据，比如文件路径
+                }
+                else
+                {
+                    e.Effect = DragDropEffects.None;
+                }
+            }
+            catch (Exception ex)
+            {
+                // 在这里处理异常
+                MessageBox.Show(ex.Message);
+            }
         }
 
         /// <summary>
@@ -36,8 +48,16 @@ namespace QueryExcel
         /// <param name="e"></param>
         private void Form1_DragDrop(object sender, DragEventArgs e) //解析信息
         {
-            string path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString(); //获得路径
-            textBox1.Text = path; //由一个textBox显示路径
+            try
+            {
+                string path = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString(); //获得路径
+                textBox1.Text = path; //由一个textBox显示路径
+            }
+            catch (Exception ex)
+            {
+                // 在这里处理异常
+                MessageBox.Show(ex.Message);
+            }
         }
 
         /// <summary>
@@ -47,21 +67,29 @@ namespace QueryExcel
         /// <param name="e"></param>
         private void TextBox1_TextChanged(object sender, EventArgs e)
         {
-            TextBox textBox = textBox1;
-            string filePath = textBox.Text;
-            if (string.IsNullOrEmpty(filePath) == false)
+            try
             {
-                string fileType = Path.GetExtension(filePath);
-                if (fileType == ".xls" || fileType == ".xlsx")
+                TextBox textBox = textBox1;
+                string filePath = textBox.Text;
+                if (string.IsNullOrEmpty(filePath) == false)
                 {
-                    // 如果是Excel文件路径，执行相应的操作
-                    previousText = filePath; // 保存当前的值，以便下次使用
+                    string fileType = Path.GetExtension(filePath);
+                    if (fileType == ".xls" || fileType == ".xlsx")
+                    {
+                        // 如果是Excel文件路径，执行相应的操作
+                        previousText = filePath; // 保存当前的值，以便下次使用
+                    }
+                    else
+                    {
+                        // 如果不是Excel文件路径，执行相应的操作
+                        textBox.Text = previousText; // 将文本框的值重置为上一次的值
+                    }
                 }
-                else
-                {
-                    // 如果不是Excel文件路径，执行相应的操作
-                    textBox.Text = previousText; // 将文本框的值重置为上一次的值
-                }
+            }
+            catch (Exception ex)
+            {
+                // 在这里处理异常
+                MessageBox.Show(ex.Message);
             }
         }
 
@@ -72,8 +100,8 @@ namespace QueryExcel
         /// <param name="e"></param>
         private void Button1_Click(object sender, EventArgs e)
         {
-            //try
-            //{
+            try
+            {
                 // 在这里放置可能会抛出异常的代码
 
                 //获取文件路径
@@ -97,12 +125,12 @@ namespace QueryExcel
                         listBox1.Items.Add(dataSet1.Tables[i].TableName);
                     }
                 }
-            //}
-            //catch (Exception ex)
-            //{
-            //    // 在这里处理异常
-            //    MessageBox.Show(ex.Message);
-            //}
+            }
+            catch (Exception ex)
+            {
+                // 在这里处理异常
+                MessageBox.Show(ex.Message);
+            }
         }
 
         /// <summary>
@@ -112,28 +140,36 @@ namespace QueryExcel
         /// <param name="e"></param>
         private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            ListBox lb = sender as ListBox; // 声明ListBox控件事件触发对象
-
-            string tableName = lb.SelectedItem.ToString();// 获取listbox的选中值
-
-            if (dataSet1.Tables.Contains(tableName)) // 检查DataSet中是否存在这个DataTable
+            try
             {
-                this.dataGridView1.DataSource = null;
-                this.dataGridView1.DataSource = dataSet1.Tables[tableName]; // 将DataTable绑定到DataGridView
+                ListBox lb = sender as ListBox; // 声明ListBox控件事件触发对象
 
-                // 将Datable中所有列名绑定到bindingSource
-                List<string> columnNames = new();
-                foreach (DataColumn column in dataSet1.Tables[tableName].Columns)
+                string tableName = lb.SelectedItem.ToString();// 获取listbox的选中值
+
+                if (dataSet1.Tables.Contains(tableName)) // 检查DataSet中是否存在这个DataTable
                 {
-                    columnNames.Add(column.ColumnName);
-                }
+                    this.dataGridView1.DataSource = null;
+                    this.dataGridView1.DataSource = dataSet1.Tables[tableName]; // 将DataTable绑定到DataGridView
 
-                this.bindingSource1.DataSource = null;
-                this.bindingSource1.DataSource = columnNames;
+                    // 将Datable中所有列名绑定到bindingSource
+                    List<string> columnNames = new();
+                    foreach (DataColumn column in dataSet1.Tables[tableName].Columns)
+                    {
+                        columnNames.Add(column.ColumnName);
+                    }
+
+                    this.bindingSource1.DataSource = null;
+                    this.bindingSource1.DataSource = columnNames;
+                }
+                else
+                {
+                    MessageBox.Show("未找到名为 " + tableName + " 的DataTable");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                MessageBox.Show("未找到名为 " + tableName + " 的DataTable");
+                // 在这里处理异常
+                MessageBox.Show(ex.Message);
             }
         }
 
@@ -144,8 +180,16 @@ namespace QueryExcel
         /// <param name="e"></param>
         private void Button4_Click(object sender, EventArgs e)
         {
-            ExcelHandler excelHandler = new();
-            excelHandler.ExportExcel(dataGridView3);
+            try
+            {
+                ExcelHandler excelHandler = new();
+                excelHandler.ExportExcel(dataGridView3);
+            }
+            catch (Exception ex)
+            {
+                // 在这里处理异常
+                MessageBox.Show(ex.Message);
+            }
         }
 
         /// <summary>
@@ -166,96 +210,116 @@ namespace QueryExcel
         /// <param name="e"></param>
         private void Button2_Click(object sender, EventArgs e)
         {
-            /*
-             代码调试中......
-             */
-
-            string filter = ""; //过滤条件字符串
-
-            //遍历每一行,但不包括最后新建行 || 自定义筛选条件
-            foreach (DataGridViewRow row in dataGridView2.Rows)
+            try
             {
-                if (!row.IsNewRow)
+
+                int lastRowIndex = dataGridView2.Rows.Count - 2;
+
+                if (lastRowIndex > 0)
                 {
-                    // 获取列的值
-                    string Value1 = (string)row.Cells["Column1"].Value;
-                    string Value2 = (string)row.Cells["Column2"].Value;
-                    string Value3 = (string)row.Cells["Column3"].Value;
-                    string Value4 = (string)row.Cells["Column4"].Value;
-                    string Value5 = (string)row.Cells["Column5"].Value;
-
-                    //MessageBox.Show($"{Value1} - {Value2} - {Value3} - {Value4} - {Value5}");
-
-                    // 处理字符串,根据比较运算符定义查询字符串
-                    if (!string.IsNullOrEmpty(Value2))
+                    for (int i = 0; i < lastRowIndex; i++)
                     {
-                        // 使用switch语句
-                        switch (Value2)
+                        string Value5 = (string)dataGridView2.Rows[i].Cells["Column5"].Value;
+                        if (string.IsNullOrEmpty(Value5))
                         {
-                            case "等于":
-                                filter += string.Format("{0} = '{1}'", Value1, Value3);
-                                break;
-                            case "不等于":
-                                filter += string.Format("{0} <> '{1}'", Value1, Value3);
-                                break;
-                            case "包含":
-                                filter += string.Format("({0} LIKE '%{1}%')", Value1, Value3);
-                                break;
-                            case "不包含":
-                                filter += string.Format("({0} NOT LIKE '%{1}%')", Value1, Value3);
-                                break;
-                            case "大于等于":
-                                filter += string.Format("({0} >= '{1}')", Value1, Value3);
-                                break;
-                            case "小于等于":
-                                filter += string.Format("({0} <= '{1}')", Value1, Value3);
-                                break;
-                            case "大于":
-                                filter += string.Format("({0} > '{1}')", Value1, Value3);
-                                break;
-                            case "小于":
-                                filter += string.Format("({0} < '{1}')", Value1, Value3);
-                                break;
-                            case "在...之内":
-                                filter += string.Format("({0} >= '{1}' AND {0} <= '{2}')", Value1, Value3, Value4);
-                                break;
-                            case "在...之外":
-                                filter += string.Format("({0} < '{1}' OR {0} > '{2}')", Value1, Value3, Value4);
-                                break;
-                            // 如果columnName不等于上述任何值
-                            default:
-                                break;
-                        }
-                    }
-                    //根据行数判断是否添加多条件连接符
-                    if (!string.IsNullOrEmpty(Value5))
-                    {
-                        // 使用switch语句
-                        switch (Value5)
-                        {
-                            case "且":
-                                filter = $"{filter} AND ";
-                                break;
-                            case "或":
-                                filter = $"{filter} OR ";
-                                break;
-                            // 如果columnName不等于上述任何值
-                            default:
-                                break;
+                            MessageBox.Show("多条件筛选数据时，‘多条件连接符’不可省略。");
+                            return;
                         }
                     }
                 }
+
+                string filter = ""; //过滤条件字符串
+
+                //遍历每一行,但不包括最后新建行 || 自定义筛选条件
+                foreach (DataGridViewRow row in dataGridView2.Rows)
+                {
+                    if (!row.IsNewRow)
+                    {
+                        // 获取列的值
+                        string Value1 = (string)row.Cells["Column1"].Value;
+                        string Value2 = (string)row.Cells["Column2"].Value;
+                        string Value3 = (string)row.Cells["Column3"].Value;
+                        string Value4 = (string)row.Cells["Column4"].Value;
+                        string Value5 = (string)row.Cells["Column5"].Value;
+
+                        //MessageBox.Show($"{Value1} - {Value2} - {Value3} - {Value4} - {Value5}");
+
+                        // 处理字符串,根据比较运算符定义查询字符串
+                        if (!string.IsNullOrEmpty(Value2))
+                        {
+                            // 使用switch语句
+                            switch (Value2)
+                            {
+                                case "等于":
+                                    filter += string.Format("{0} = '{1}'", Value1, Value3);
+                                    break;
+                                case "不等于":
+                                    filter += string.Format("{0} <> '{1}'", Value1, Value3);
+                                    break;
+                                case "包含":
+                                    filter += string.Format("({0} LIKE '%{1}%')", Value1, Value3);
+                                    break;
+                                case "不包含":
+                                    filter += string.Format("({0} NOT LIKE '%{1}%')", Value1, Value3);
+                                    break;
+                                case "大于等于":
+                                    filter += string.Format("({0} >= '{1}')", Value1, Value3);
+                                    break;
+                                case "小于等于":
+                                    filter += string.Format("({0} <= '{1}')", Value1, Value3);
+                                    break;
+                                case "大于":
+                                    filter += string.Format("({0} > '{1}')", Value1, Value3);
+                                    break;
+                                case "小于":
+                                    filter += string.Format("({0} < '{1}')", Value1, Value3);
+                                    break;
+                                case "在...之内":
+                                    filter += string.Format("({0} >= '{1}' AND {0} <= '{2}')", Value1, Value3, Value4);
+                                    break;
+                                case "在...之外":
+                                    filter += string.Format("({0} < '{1}' OR {0} > '{2}')", Value1, Value3, Value4);
+                                    break;
+                                // 如果columnName不等于上述任何值
+                                default:
+                                    break;
+                            }
+                        }
+                        //根据行数判断是否添加多条件连接符
+                        if (!string.IsNullOrEmpty(Value5))
+                        {
+                            // 使用switch语句
+                            switch (Value5)
+                            {
+                                case "且":
+                                    filter = $"{filter} AND ";
+                                    break;
+                                case "或":
+                                    filter = $"{filter} OR ";
+                                    break;
+                                // 如果columnName不等于上述任何值
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                }
+
+                //获取DataTable数据
+                string tableName = listBox1.SelectedItem.ToString();
+
+                Query query = new();
+                DataTable dataTable = query.Select(dataSet1.Tables[tableName], filter);
+
+                //开始过滤数据
+                dataGridView3.DataSource = null;
+                dataGridView3.DataSource = dataTable;
             }
-
-            //获取DataTable数据
-            string tableName = listBox1.SelectedItem.ToString();
-
-            Query query = new();
-            DataTable dataTable = query.Select(dataSet1.Tables[tableName], filter);
-
-            //开始过滤数据
-            dataGridView3.DataSource = null;
-            dataGridView3.DataSource = dataTable;
+            catch (Exception ex)
+            {
+                // 在这里处理异常
+                MessageBox.Show(ex.Message);
+            }
         }
 
         /// <summary>
